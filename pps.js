677f34171351ae5bae0ed4a8d52d7dfd32dadd10
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-08-06 23:17:07 +0100

    first commit

---
+++ pps.js
@@ -0,0 +1,59 @@
+// Module 16 - Javascript practice problem
+
+//1. Shihan's dad gave him 500tk and asked him to buy some 2kg orange and 3kg apples. The price for fruits is 56tk and 59tk per kg. Write a program to help Shahan calculate how much money the shopkeeper will return.
+
+// Solution1:
+
+// var totalMoney = 500;
+// var applePrice = 60;
+// var orangePrice = 50;
+
+// var totalApplePrice = applePrice * 3;
+// var totalOrangePrice = orangePrice * 2;
+// var totalCost = totalApplePrice + totalOrangePrice;
+
+// var remainingMoney = totalMoney - totalCost;
+
+// // console.log(remainingMoney);
+
+// ************************************************************************
+
+// 2. Write a program to calculate the average marks of Mathematics,Biology,Chemistry,Physics and Bangla of a student.
+
+// Solution2:
+
+// var Mathematics = 80;
+// var Biology = 70;
+// var Chemistry = 75;
+// var Physics = 85;
+// var Bangla = 90;
+
+// var totalMarks = Mathematics + Biology + Chemistry + Physics + Bangla;
+// var average = totalMarks / 5;
+
+// console.log(average);
+
+// ************************************************************************
+
+// 3. Shihan's teacher gave him two variables contains of string. Shihan's teacher asked him to combine these two strings and print them in one line. Help Shihan to write the program.
+
+// Solution3:
+
+// var string1 = "I am going to be";
+// var string2 = "an awesome web developer";
+
+// var fullString = string1 + " " + string2;
+
+// console.log(fullString);
+
+// ************************************************************************
+
+// 4. Shihan's mom is teaching him Mathematics. She gave Shihan a number 17 and asked him what the reminder would be if she divided the number by 5. Write a program to help Shihan to solve this.
+
+// Solution4:
+
+var givenNumber = 17;
+var dividend = 5;
+
+var reminder = givenNumber % dividend;
+console.log(reminder);
---
+++ variable.js
@@ -0,0 +1,16 @@
+// vary +able = variable
+
+var price = 39;
+var age = 27;
+var weight = 50;
+var height = 153;
+var temperature = 34;
+
+// in case of adding float number we can detemine the decimal value by following.
+
+var price1 = .1;
+var price = .2;
+var sum = price1 + price;
+// sum = sum.toFixed(1);
+// sum = parseFloat(sum);
+console.log(sum);
